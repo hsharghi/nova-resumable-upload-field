5736a6e95a37d878cbc454582998946bc4410fac
Hadi Sharghi <hsharghi@gmail.com>
2019-07-30 12:18:06 +0430

    fixed uploader require name

--- resources/js/field.js
+++ resources/js/field.js
@@ -2,7 +2,7 @@ import * as uploader from "vue-simple-uploader";
 
 Nova.booting((Vue, router, store) => {
     Vue.use(uploader);
-    Vue.component('uploader-component', require('./components/UploaderComponent.vue'));
+    Vue.component('uploader-component', require('./components/UploaderComponent.vue'))
     Vue.component('index-resumable-upload-field', require('./components/IndexField'))
     Vue.component('detail-resumable-upload-field', require('./components/DetailField'))
     Vue.component('form-resumable-upload-field', require('./components/FormField'))
