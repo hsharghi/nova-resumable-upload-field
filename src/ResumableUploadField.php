0d6d668eb8807c4c20d2ce6ff11fc36f78cad6dc
Hadi Sharghi <hsharghi@gmail.com>
2019-07-30 13:03:37 +0430

    add apiUrl and uploadUrl properties to Field

--- src/ResumableUploadField.php
+++ src/ResumableUploadField.php
@@ -12,4 +12,22 @@ class ResumableUploadField extends Field
      * @var string
      */
     public $component = 'resumable-upload-field';
+
+//    public $uploadUrl = '';
+//    public $apiUrl = '';
+
+    public function apiUrl($url)
+    {
+        return $this->withMeta([
+            'apiUrl' => $url,
+        ]);
+    }
+
+
+    public function uploadUrl($url)
+    {
+        return $this->withMeta([
+            'uploadUrl' => $url,
+        ]);
+    }
 }
