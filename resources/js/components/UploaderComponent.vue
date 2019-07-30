2e8f0280285aaa9a4c5ee423e2de653ef13e451f
Hadi Sharghi <hsharghi@gmail.com>
2019-07-30 13:03:53 +0430

    remove upload folder button

--- resources/js/components/UploaderComponent.vue
+++ resources/js/components/UploaderComponent.vue
@@ -1,16 +1,15 @@
 <template>
-    <div>
-        <uploader :options="options" class="uploader-example">
-            <uploader-unsupport></uploader-unsupport>
-            <uploader-drop>
-                <p>Drop files here to upload or</p>
-                <uploader-btn :attrs="attrs">select files</uploader-btn>
-                <!--<uploader-btn :attrs="attrs">select images</uploader-btn>-->
-                <uploader-btn :directory="true">select folder</uploader-btn>
-            </uploader-drop>
-            <uploader-list></uploader-list>
-        </uploader>
-    </div>
+    <uploader :options="options" class="uploader-example">
+        <uploader-unsupport></uploader-unsupport>
+        <uploader-drop>
+            <p>Drop files here to upload or</p>
+            <br/>
+            <uploader-btn :attrs="attrs">select files</uploader-btn>
+            <!--<uploader-btn :attrs="attrs">select images</uploader-btn>-->
+            <!--<uploader-btn :directory="true">select folder</uploader-btn>-->
+        </uploader-drop>
+        <uploader-list></uploader-list>
+    </uploader>
 </template>
 
 <script>
